4647318bb8603e67bc6f4d14f609860b772fc6d9
Prishi Amin <prishiamin8@gmail.com>
2024-11-19 22:18:58 -0500

    all done

--- project_3/main.cpp
+++ project_3/main.cpp
@@ -1,63 +1,26 @@
+#include "ece250_socket.h"
 #include <iostream>
-#include "Trie.h" // Include the Trie header file
-using namespace std;
+#include <sstream> //new include!
 
-int main() {
-    Trie trie;   // Create an object of the Trie class
-    string command;   // Store input command
+int main()
+{
+	//Just for fun, let's reclassify "Dog"
+	std::string text_to_classify = "Dog";
+	std::string candidate_labels = "animal,plant,technology,no thing";
+	std::cout << labelText(text_to_classify,candidate_labels) << std::endl;
 
-    while (cin >> command) {
-        if (command == "LOAD") {
-            string filename;
-            cin >> filename;
-
-            trie.load_input(filename);
-            cout << "success" << endl;
-        }
-
-        else if (command == "INSERT") {
-            string classification;
-            cin >> classification;
-            trie.insert_classification(classification);
-        }
-
-        else if (command == "CLASSIFY") {
-            string input;
-            cin >> input;
-            trie.classify_input(input);
-        }
-
-        else if (command == "ERASE") {
-            string classification;
-            cin >> classification;
-            trie.erase_classification(classification);
-        }
-
-        else if (command == "PRINT") {
-            trie.print_classifications();
-        }
-
-        else if (command == "EMPTY") {
-            if (trie.is_trie_empty()) {
-                cout << "empty 1" << endl;
-            }
-            else {
-                cout << "empty 0" << endl;
-            }
-        }
-
-        else if (command == "CLEAR") {
-            trie.clear_all_nodes();
-        }
-
-        else if (command == "SIZE") {
-            cout << "number of classifications is " << trie.return_trie_size() << endl;
-        }
-
-        else if (command == "EXIT") {
-            break;
-        }
-    }
-
-    return 0;
+	//OK, now let's split a string. In fact, let's split candidate_labels
+	
+	//step 1: make a new stringstring with the contents of our candidate_labels 
+	std::istringstream stream(candidate_labels); //I could have named it anything, like stringin or something
+	
+	//step 2: make a temporary string that we will use to store the extracted parts
+	std::string single_label;
+	
+	//step 3: use the getline function with the comma sent in as a delimiter to read the string parts
+	while(std::getline(stream,single_label,',')){
+		std::cout << single_label << std::endl; //and let's print it just to make sure it worked
+	}
+	
+	return 0;
 }
---
+++ project_3/trietest.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "Trie.h" // Include the Trie header file
+using namespace std;
+
+int main() {
+    Trie trie;   // Create an object of the Trie class
+    string command;   // Store input command
+
+    while (cin >> command) {
+        if (command == "LOAD") {
+            string filename;
+            cin >> filename;
+
+            trie.load_input(filename);
+            cout << "success" << endl;
+        }
+
+        else if (command == "INSERT") {
+            string classification;
+            cin >> classification;
+            trie.insert_classification(classification);
+        }
+
+        else if (command == "CLASSIFY") {
+            string input;
+            cin >> input;
+            trie.classify_input(input);
+        }
+
+        else if (command == "ERASE") {
+            string classification;
+            cin >> classification;
+            trie.erase_classification(classification);
+        }
+
+        else if (command == "PRINT") {
+            trie.print_classifications();
+        }
+
+        else if (command == "EMPTY") {
+            if (trie.is_trie_empty()) {
+                cout << "empty 1" << endl;
+            }
+            else {
+                cout << "empty 0" << endl;
+            }
+        }
+
+        else if (command == "CLEAR") {
+            trie.clear_all_nodes();
+        }
+
+        else if (command == "SIZE") {
+            cout << "number of classifications is " << trie.return_trie_size() << endl;
+        }
+
+        else if (command == "EXIT") {
+            break;
+        }
+    }
+
+    return 0;
+}
