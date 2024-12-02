2bdcf2f84911812ad562f42006bea5c13387846b
Prishi Amin <prishiamin8@gmail.com>
2024-12-02 03:09:58 -0500

    class methods complete

--- project_4/Node_Entity.cpp
+++ project_4/Node_Entity.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // Constructor definition
 Node_Entity::Node_Entity() {
-    edges_list = vector < tuple < Node_Entity* , double, string > > ();   // Holds all edges (adjacent nodes)
+    edges_list = vector < tuple < Node_Entity * , double, string > > ();   // Holds all edges (adjacent nodes)
     string entity_id = "";   // entity node id
     string entity_name = ""; // entity node name
     string entity_type = ""; // entity node type
@@ -18,7 +18,7 @@ Node_Entity::~Node_Entity() {
 
 
 // Get list of edges (adjacent nodes) for current node
-vector < tuple < Node_Entity* , double, string > > Node_Entity::get_edges() {
+vector < tuple < Node_Entity * , double, string > > Node_Entity::get_edges() {
     return edges_list;
 }
 
@@ -26,16 +26,21 @@ vector < tuple < Node_Entity* , double, string > > Node_Entity::get_edges() {
 
 // Insert edge between current and an adjacent node
     // Create a tuple of: pointer to adjacent node, edge's weight, edge's label
-    // Add this tuple to edges_list
-void Node_Entity::insert_edge(Node_Entity* adjacent_node, double weight, string label) {
-
+    // Add tuple to edges_list
+void Node_Entity::insert_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label) {
+    edges_list.push_back(make_tuple(adjacent_node, edge_weight, edge_label)); // Add adjacent node to edges list
 }
 
 
 // Delete edge between current and an adjacent node
     // Find and delete adjacent node's tuple from edges_list of current node
-void Node_Entity::delete_edge(Node_Entity* adjacent_node) {
-
+void Node_Entity::delete_edge(Node_Entity * adjacent_node) {
+    for (int i = 0; i < edges_list.size(); i++) {
+        if (get<0>(edges_list[i]) == adjacent_node) {   // if 1st element in tuple at index i in edges_list == adjacent node
+            edges_list.erase(edges_list.begin() + i);  // Remove adjacent node from index i (this removes the edge)
+            break;
+        }
+    }
 }
 
 
--- project_4/Node_Entity.h
+++ project_4/Node_Entity.h
@@ -8,7 +8,7 @@ using namespace std;
 
 class Node_Entity {
 private:
-    vector < tuple < Node_Entity* , double, string > > edges_list;  // Vector contains the edges of a node
+    vector < tuple < Node_Entity * , double, string > > edges_list; // Vector contains the edges of a node
                                                                     // tuple contains: pointer to adjacent node,
                                                                     //                 edge's weight,
                                                                     //                 edge's label
@@ -20,14 +20,14 @@ public:
     Node_Entity();   // Constructor
     ~Node_Entity();  // Destructor
 
-    vector < tuple < Node_Entity* , double, string > > get_edges();    // Get all of the edges (adjacent nodes)
-    void insert_edge(Node_Entity* adjacent_node, double weight, string label); // Insert edge b/w current and adjacent node
+    vector < tuple < Node_Entity * , double, string > > get_edges();    // Get all of the edges (adjacent nodes)
+    void insert_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label); // Insert edge b/w current & adjacent node
                                                                                   // Create a tuple of:
                                                                                   //    pointer to adjacent node,
                                                                                   //    edge's weight,
                                                                                   //    edge's labe;
                                                                                   // Add this tuple to edges_list
-    void delete_edge(Node_Entity* adjacent_node);   // Delete edge between current and adjacent node
+    void delete_edge(Node_Entity * adjacent_node);   // Delete edge between current and adjacent node
                                                     // Find and delete adjacent node's tuple from edges_list of current node
 
     // For accessing private variables in the Graph class:
