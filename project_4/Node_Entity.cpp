03c94d51bae6a263c14a78985dd1165f91cdde71
Prishi Amin <prishiamin8@gmail.com>
2024-12-02 14:41:24 -0500

    new functions added

--- project_4/Node_Entity.cpp
+++ project_4/Node_Entity.cpp
@@ -6,6 +6,7 @@ using namespace std;
 // Constructor definition
 Node_Entity::Node_Entity() {
     edges_list = vector < tuple < Node_Entity * , double, string > > ();   // Holds all edges (adjacent nodes)
+                                                                           // tuple: pointer to adjacent node, edge's weight, edge's label
     string entity_id = "";   // entity node id
     string entity_name = ""; // entity node name
     string entity_type = ""; // entity node type
@@ -14,21 +15,27 @@ Node_Entity::Node_Entity() {
 
 // Destructor definition
 Node_Entity::~Node_Entity() { 
+    edges_list.clear();
 }
 
 
 // Get list of edges (adjacent nodes) for current node
-vector < tuple < Node_Entity * , double, string > > Node_Entity::get_edges() {
+vector < tuple < Node_Entity * , double, string > > Node_Entity::get_edges_list() {
     return edges_list;
 }
 
 
-
-// Insert edge between current and an adjacent node
+// Insert or update (weight & label) edge between current & adjacent node
     // Create a tuple of: pointer to adjacent node, edge's weight, edge's label
     // Add tuple to edges_list
-void Node_Entity::insert_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label) {
-    edges_list.push_back(make_tuple(adjacent_node, edge_weight, edge_label)); // Add adjacent node to edges list
+void Node_Entity::set_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label) {
+    for (int i= 0; i < edges_list.size(); i++) {
+        if (get<0>(edges_list[i]) == adjacent_node) {  // Iterate through adjacent nodes (index 0 of tuple = adjacent node)
+            edges_list[i] = make_tuple(adjacent_node, edge_weight, edge_label);// Edge exists (destination is an adjacent node) = update weight & label
+            return;
+        }
+    }
+    edges_list.push_back(make_tuple(adjacent_node, edge_weight, edge_label)); // Edge DNE, so add new adjacent node to edges list
 }
 
 
@@ -38,7 +45,7 @@ void Node_Entity::delete_edge(Node_Entity * adjacent_node) {
     for (int i = 0; i < edges_list.size(); i++) {
         if (get<0>(edges_list[i]) == adjacent_node) {   // if 1st element in tuple at index i in edges_list == adjacent node
             edges_list.erase(edges_list.begin() + i);  // Remove adjacent node from index i (this removes the edge)
-            break;
+            return;
         }
     }
 }
--- project_4/Node_Entity.h
+++ project_4/Node_Entity.h
@@ -9,9 +9,7 @@ using namespace std;
 class Node_Entity {
 private:
     vector < tuple < Node_Entity * , double, string > > edges_list; // Vector contains the edges of a node
-                                                                    // tuple contains: pointer to adjacent node,
-                                                                    //                 edge's weight,
-                                                                    //                 edge's label
+                                                                    // tuple contains: pointer to adjacent node, edge's weight, edge's label
     string entity_id;   // entity node id
     string entity_name; // entity node name
     string entity_type; // entity node type
@@ -20,17 +18,17 @@ public:
     Node_Entity();   // Constructor
     ~Node_Entity();  // Destructor
 
-    vector < tuple < Node_Entity * , double, string > > get_edges();    // Get all of the edges (adjacent nodes)
-    void insert_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label); // Insert edge b/w current & adjacent node
-                                                                                  // Create a tuple of:
-                                                                                  //    pointer to adjacent node,
-                                                                                  //    edge's weight,
-                                                                                  //    edge's labe;
-                                                                                  // Add this tuple to edges_list
+    void set_edge(Node_Entity * adjacent_node, double edge_weight, string edge_label); // Insert or update (weight & label) edge between current & adjacent node
+                                                                                        // Create a tuple of:
+                                                                                        //    pointer to adjacent node, edge's weight, edge's labe;
+                                                                                        // Add this tuple to edges_list
+
     void delete_edge(Node_Entity * adjacent_node);   // Delete edge between current and adjacent node
                                                     // Find and delete adjacent node's tuple from edges_list of current node
 
-    // For accessing private variables in the Graph class:
+    // For accessing private members from this class when working in the Graph class:
+    vector < tuple < Node_Entity * , double, string > > get_edges_list();    // Get all of the edges (adjacent nodes) for node
+
     string get_entity_id();    // get entity node's id
     void set_entity_id(string new_id);    // update/set entity node's id
     
